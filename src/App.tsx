9c07840921c0149b1bee7c3ada4c911a61f241b6
Jecé Xavier <jece@alunos.utfpr.edu.br>
2022-12-22 22:21:45 +0000

    feat: list data from database

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,72 @@
-function App() {
-  return <h1>Hello Word!!</h1>;
-}
+import { useEffect, useState } from "react";
+import { useReadCypher } from "use-neo4j";
 
-export default App;
+export const App: React.FC = () => {
+  const [diseases, setDisease] = useState([]);
+  const [symptoms, setSymptoms] = useState([]);
+
+  const { loading, records, run, error, result } =
+    useReadCypher("MATCH (n) RETURN n");
+
+  if (loading) return <div>Loading...</div>;
+
+  if (error) return <div className="error">{error.message}</div>;
+
+  // const itens = result?.records.map((row) => row.get("n"));
+  // console.log(itens);
+
+  if (result) {
+    const data = result?.records
+      .map((row) => row.get("n"))
+      .reduce(
+        (accumulator, currentValue) => {
+          // console.log(currentValue);
+          if (currentValue.labels.includes("Symptom")) {
+            return {
+              ...accumulator,
+              symptom: [...accumulator.symptom, currentValue.properties.name],
+            };
+          } else if (currentValue.labels.includes("Disease")) {
+            return {
+              ...accumulator,
+              disease: [...accumulator.disease, currentValue.properties.name],
+            };
+          }
+          return {};
+        },
+        {
+          symptom: [],
+          disease: [],
+        }
+      );
+
+    // setSymptoms(data.symptom);
+    // setDisease(data.disease);
+
+    return (
+      <>
+        <h1>DATA</h1>
+        <div style={{ display: "flex" }}>
+          symptom
+          <ul>
+            {data.symptom.map((s) => (
+              <li>{s}</li>
+            ))}
+          </ul>
+          disease
+          <ul>
+            {data.disease.map((d) => (
+              <li>{d}</li>
+            ))}
+          </ul>
+        </div>
+      </>
+    );
+  } else {
+    return <div>sem result</div>;
+  }
+
+  // useEffect(() => {
+  //   setDisease([]);
+  // }, []);
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { App } from "./App";
+
+import { createDriver, Neo4jProvider } from "use-neo4j";
+
+const driver = createDriver(
+  "neo4j",
+  "localhost",
+  7687,
+  "neo4j",
+  "project_ws",
+  {}
+);
+
+console.log(driver);
+//database="symptom_disease"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Neo4jProvider driver={driver}>
+      <App />
+    </Neo4jProvider>
   </React.StrictMode>
 );
